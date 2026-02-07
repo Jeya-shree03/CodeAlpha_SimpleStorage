c7f49ab1d09f45bdeda1490c5deefa881a7fb79f
Jeya-shree03 <shreejeya345@gmail.com>
2026-02-07 15:05:56 +0530

    simple Storage smart Contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,14 @@
+
+
+pragma solidity ^0.8.0;
+contract SimpleStorage{
+    uint public value;
+    function increment() public{
+        value+=1;
+
+    }
+    function decrement() public{
+        value-=1;
+    }
+}
+
